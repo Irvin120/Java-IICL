9832dda5b6e8cf37c86c0db88b0b6062c6c45c79
Irvin Cueto <iirvin.cueto.02@gmail.com>
2023-09-21 21:16:56 -0600

    Excel JSPs con Directivas

--- Servlets/EjemploExcelJSP/src/main/java/utilerias/Conversiones.java
+++ Servlets/EjemploExcelJSP/src/main/java/utilerias/Conversiones.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utilerias;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
  * @author Irvin Cueto
  */
 public class Conversiones {
+    private static final String FORMATO_FECHA = "dd-MM";
+    
+    public static String format (Date fecha){
+        SimpleDateFormat fomateador = new SimpleDateFormat(FORMATO_FECHA);
+        
+        return fomateador.format(fecha);
+    }
     
 }
